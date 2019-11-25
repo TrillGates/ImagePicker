38d9eb9b9c9b1285252902e2d088544dcd34eb0b
849077547 <849077547@qq.com>
2019-11-25 13:38:34 +0800

    update main actvity.

--- app/src/main/java/com/sunofbeaches/imagepicker/MainActivity.java
+++ app/src/main/java/com/sunofbeaches/imagepicker/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements PickerConfig.OnIm
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
         checkPermission();
         initPickerConfig();
     }
